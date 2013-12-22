8272d0890b6fd87e7316cd95b94470e855a9146c
Alexey Ivanov <amivanoff@gmail.com>
2013-12-22 21:50:34 +0400

    Powerloom tests fix

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 powerloom-osgi
 ==============
+
+Installation
+
+Add castor.jar and castor-core.jar from PowerLoom 4.0.6 distro to the plugins\edu.isi.powerloom\lib\ directory
+
+Project edu.isi.powerloom.server didn't compiles yet
---
+++ tests/edu.isi.powerloom.tests/src/edu/isi/powerloom/tests/StellajTest.java
@@ -0,0 +1,32 @@
+package edu.isi.powerloom.tests;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.pl.stellaj.InitHandler;
+import org.pl.stellaj.Stellaj;
+
+public class StellajTest {
+
+	@Test
+	public void test() {
+		
+		Stellaj stellaj = Stellaj.getInstance();
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		
+		Stellaj stellaj = Stellaj.getInstance();
+		stellaj.addInitializer(Stellaj.STAGES.BEGIN, 0, new InitHandler() {
+			
+			@Override
+			public void handle() {
+				System.out.println("Hello world!!!");
+			}
+		});
+	}
+
+}
