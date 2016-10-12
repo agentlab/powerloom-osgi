1ded9fd2b61db59eed5289d1e09abb3988bac6aa
Alex Ivanov <amivanoff@gmail.com>
2016-10-12 14:58:15 +0300

    Tests fix

--- tests/edu.isi.powerloom.tests/src/edu/isi/powerloom/tests/RuEncodingTest.java
+++ tests/edu.isi.powerloom.tests/src/edu/isi/powerloom/tests/RuEncodingTest.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
-import org.powerloom.PowerLoomService;
+import org.powerloom.service.PowerLoomService;
 
 import edu.isi.powerloom.PLI;
 import edu.isi.powerloom.PlIterator;
@@ -12,7 +12,7 @@ import edu.isi.powerloom.logic.LogicObject;
 import edu.isi.stella.Module;
 
 public class RuEncodingTest {
-	
+
 	public static final String CONCEPT_NAME = "Concept";
 //	public static final String MODULE_NAME_KERNEL = "PL-KERNEL";
 	public static final String MODULE_NAME_TEST = "TEST";
@@ -20,16 +20,16 @@ public class RuEncodingTest {
 	@Test
 	public void test() {
 		PowerLoomService pls = PowerLoomService.getInstance();
-		
+
 		try {
 			pls.loadPlm("RuEncodingTest.plm");
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-		
+
 		//Module moduleKernel = PLI.getModule(MODULE_NAME_KERNEL, null);
 		Module moduleTest = PLI.getModule(MODULE_NAME_TEST, null);
-		
+
 		//String query = "all ?x (Concept ?x)";
 		String query = "all (and (UseCase ?x) (hasTitle ?x ?y))";
 		PlIterator answer = PLI.sRetrieve(query, MODULE_NAME_TEST, null);
@@ -38,9 +38,9 @@ public class RuEncodingTest {
 	    while (answer.nextP()) {
 	      System.out.println(answer.value/* + " " + ((Cons)answer.value).rest.value*/);
 	    }
-	    
-	    System.out.println();		
-	    
+
+	    System.out.println();
+
 	    LogicObject concept = PLI.getConcept(CONCEPT_NAME, moduleTest, null);
 		PlIterator iter = PLI.getConceptInstances(concept, moduleTest, null);
 		while (iter.nextP()) {
@@ -51,7 +51,7 @@ public class RuEncodingTest {
 				trim = trim.replaceAll("\\|", "");
 			}
 			LogicObject sd = PLI.getConcept(trim, moduleTest, null);
-			
+
 			System.out.println(trim + " " + moduleTest + " " + sd);
 //			if (PLI.getHomeModule(sd).equals(module)){
 				//l.add(trim);
